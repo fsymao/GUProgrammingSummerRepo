244910f591b9379ebd5fa4c8ea257c4db03bd124
fsymao <fsymao@gmail.com>
2018-06-07 15:14:42 +0800

    Initial Commit
    Initial Commit

---
+++ hw1/HW1part1.R
@@ -0,0 +1,62 @@
+####Week1 Assignment#####
+
+#---------------------Information about the dataset-----------------------------------------------------------------#
+# Source:
+#   
+# Data comes from an original (non-machine-learning) study: 
+# Warwick J Nash, Tracy L Sellers, Simon R Talbot, Andrew J Cawthorn and Wes B Ford (1994) 
+# "The Population Biology of Abalone (_Haliotis_ species) in Tasmania. I. Blacklip Abalone (_H. rubra_) from the North Coast and Islands of Bass Strait", 
+# Sea Fisheries Division, Technical Report No. 48 (ISSN 1034-3288) 
+
+## The dataset comes from https://archive.ics.uci.edu/ml/datasets/Abalone
+
+# Data Set Information:
+#   
+# Predicting the age of abalone from physical measurements. 
+#The age of abalone is determined by cutting the shell through the cone, staining it, 
+#and counting the number of rings through a microscope -- a boring and time-consuming task. 
+#Other measurements, which are easier to obtain, are used to predict the age. 
+#Further information, such as weather patterns and location (hence food availability) may be required to solve the problem. 
+#From the original data examples with missing values were removed (the majority having the predicted value missing), 
+#and the ranges of the continuous values have been scaled.
+
+
+## Dataset Attribute Information 
+#
+# Given is the attribute name, attribute type, the measurement unit and a
+# brief description.  The number of rings is the value to predict: either
+# as a continuous value or as a classification problem.
+# 
+# Name		Data Type	Meas.	Description
+# ----		---------	-----	-----------
+#   Sex		nominal			M, F, and I (infant)
+# Length		continuous	mm	Longest shell measurement
+# Diameter	continuous	mm	perpendicular to length
+# Height		continuous	mm	with meat in shell
+# Whole weight	continuous	grams	whole abalone
+# Shucked weight	continuous	grams	weight of meat
+# Viscera weight	continuous	grams	gut weight (after bleeding)
+# Shell weight	continuous	grams	after being dried
+# Rings		integer			+1.5 gives the age in years
+#---------------------------------------------------------------------------------------------------------------------#
+
+
+
+#---------------------Actual Code Statr from Here--------------------------------------------------------------------#
+
+#read data from csv file
+data<- read.table("C:/Users/fengshaoyu/Desktop/hw1/DATASET1.csv",sep =",",
+                  header = TRUE, quote = "", stringsAsFactors = F)
+
+#---Part I (a)-----create new feature via bining
+
+
+
+
+
+
+
+
+
+
+
