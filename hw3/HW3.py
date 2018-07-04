e172f1fd12dee24580c68635c33e5a9329f04255
fsymao <fsymao@gmail.com>
2018-07-04 23:59:30 +0800

    0704

--- hw3/HW3.py
+++ hw3/HW3.py
@@ -10,9 +10,18 @@ Created on Tue Jul  3 20:39:06 2018
 import pandas as pd 
 import numpy as np
 import sklearn
-import matplotlib.pyplot
-
+import matplotlib.pyplot as plt
+from scipy import stats
+from scipy.special import stdtr
+from scipy.stats import ttest_ind, ttest_ind_from_stats
+import seaborn as sns
 #%%
+from pathlib import Path
+
+my_file = Path("OUTFILE.txt")
+if my_file.is_file():
+    open("OUTFILE.txt", "w").close()
+
 #Part1: Data Pre-processing
 
 #Read this data into Python using pandas as a dataframe.
@@ -48,11 +57,92 @@ StudentData=StudentData[(StudentData['project']>=0) & (StudentData['project']<=1
 
 print(StudentData)
 
-StudentData.to_c
+StudentData.to_csv(r'OUTFILE.txt', index=None, sep=' ', mode='a')
+
+#%%
+
+# =============================================================================
+# Using numpy - and/or python statistical methods - perform the following statistical tests on the data:
+# (a) Is there a significant difference in final scores between the two gender groups? 
+# (Run an independent samples t-test and explain the p value and result. Write all results and explanations to the OUTFILE.txt.
+# (b) Is there a correlation between the final and the project? 
+# What is the r value? Write the results and explanations to OUTFILE.txt.
+# (c) Using ANOVA, is there a significant difference between the 5 sections for final score? 
+# Create box plots to visualize the 5 sections for final score.
+# RE: http://www.marsja.se/four-ways-to-conduct-one- way-anovas-using-python/
+# =============================================================================
+#(a) Independet samples T-test 
+myfile=open("OUTFILE.txt", "a")
+myfile.write("-------------------------------------------------------------\n")
+myfile.write("------------Part 5(a)----------------------------------------\n")
+myfile.write("Null Hypothesis: There is no significant difference between  two gender groups in final score.\n ")
+myfile.write("Alternative Hypothesis: There is significant difference between  two gender groups in final score\n")
+
+## extract two data groups from pandas dataframe 
+a=StudentData[StudentData['gender']==1]['final'].values
+b=StudentData[StudentData['gender']==2]['final'].values
+
+
+##calculate the necassary stats
+abar = a.mean()
+avar = a.var(ddof=1)
+na = a.size
+adof = na - 1
+
+bbar = b.mean()
+bvar = b.var(ddof=1)
+nb = b.size
+bdof = nb - 1
+
+### calculate the t test statistics based on formula 
+### reference: https://en.wikipedia.org/wiki/Welch%27s_t-test 
+tf = (abar - bbar) / np.sqrt(avar/na + bvar/nb)
+dof = (avar/na + bvar/nb)**2 / (avar**2/(na**2*adof) + bvar**2/(nb**2*bdof))
+pf = 2*stdtr(dof, -np.abs(tf))
+myfile.write("formula:  tscore = %g  pvalue = %g \n" % (tf, pf))
+
+###verified using standard packages
+# Use scipy.stats.ttest_ind.
+t, p = ttest_ind(a, b, equal_var=False)
+myfile.write("ttest_ind:            tscore = %g  pvalue = %g \n" % (t, p))
+
+# Use scipy.stats.ttest_ind_from_stats.
+t2, p2 = ttest_ind_from_stats(abar, np.sqrt(avar), na,
+                              bbar, np.sqrt(bvar), nb,
+                              equal_var=False)
 
 
 
 
+myfile.write("ttest_ind_from_stats: tscore = %g  pvalue = %g \n" % (t2, p2))
+
+myfile.write("Given p value in this T test is 0.129, we will fail to reject Null Hypothesis. \n")
+myfile.write("In other words, there is no significant difference between gender groups in final score  \n")
+
+myfile.close()
+## ploting a kDE plot to visualize the distributuin of final scores in two gender groups
+sns.distplot(a, hist=True, kde=True, bins=5, color = 'darkblue', label = '1')
+sns.distplot(b, hist=True, kde=True, bins=5, color = 'orange', label = '2')
+plt.legend(prop={'size': 16}, title = 'GenderGroups')
+plt.title('Final Score of two Gender Groups')
+plt.xlabel('Final Scores')
+
+#%%
+myfile=open("OUTFILE.txt", "a")
+myfile.write("-------------------------------------------------------------\n")
+myfile.write("------------Part 5(b)----------------------------------------\n")
+
+#(b) Is there a correlation between the final and the project? 
+##what is the r value?
+myfile.write('Correlation coefficient matrix between two columns is:  \n')
+myfile.write(str(np.corrcoef(StudentData['final'].values,StudentData['project'].values)))
+myfile.write(' \n')
+slope, intercept, r_value, p_value, std_err = stats.linregress(StudentData['final'].values,StudentData['project'])
+myfile.write(' Linear regression formula is :  \n')
+myfile.write("Y= %g *x  + %g \n" % (slope, intercept))
+myfile.write('r-square value is :  %g \n' % r_value**2)
+myfile.write("Given r^2 value of 0.81 and pvalue of near zero, we can conclude final score and project has strong correlation")
+myfile.close()
 
 
 
